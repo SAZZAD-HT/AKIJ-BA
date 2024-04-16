d6cc210ed45668f5af155a59c42518acb5f1c5e2
sazzad <sazzadaiub1@gmail.com>
2024-04-16 13:29:46 +0600

    Add project files.

---
+++ Samurai V2 .Net 8/DbContexts/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Samurai_V2_.Net_8.DbContexts.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Mobile { get; set; }
+
+    public DateOnly? DateofBirth { get; set; }
+
+    public DateTime? ActionDate { get; set; }
+
+    public bool? IsActive { get; set; }
+}
